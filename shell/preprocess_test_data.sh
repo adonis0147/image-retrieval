e0900f45b67422919bc0c613ff13e0d6288ceb56
Adonis Ling <adonis0147@gmail.com>
2014-12-15 11:19:27 +0800

    Update shell scripts

--- shell/preprocess_test_data.sh
+++ shell/preprocess_test_data.sh
@@ -2,13 +2,13 @@
 
 mkdir -p ../data
 
-./generate_test_data.py --input=/home/adonis/Downloads/grad/split/features_jd_test.txt \
+./generate_test_data.py --input=/home/adonis/Downloads/data/features_jd_test.txt \
                         --output=../data/features_jd_test1.dat \
                         --dimension=4096
 
 echo
 
-./generate_test_data.py --input=/home/adonis/Downloads/grad/split/features_jd_test.txt \
+./generate_test_data.py --input=/home/adonis/Downloads/data/features_jd_test.txt \
                         --output=../data/features_jd_test2.dat \
                         --mapping-file=../data/mapping.dat \
                         --dimension=4096
